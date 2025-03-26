372bb8c38c1400531cd3d9ce78a7b281647cbbb2
Max Trautwein <trautwein.max@gmail.com>
2025-03-26 15:34:42 +0100

    Init Backend with java 21

---
+++ backend/focusflow/src/main/java/hse/group1/focusflow/FocusflowApplication.java
@@ -0,0 +1,13 @@
+package hse.group1.focusflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusflowApplication.class, args);
+	}
+
+}
---
+++ backend/focusflow/src/test/java/hse/group1/focusflow/FocusflowApplicationTests.java
@@ -0,0 +1,13 @@
+package hse.group1.focusflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
