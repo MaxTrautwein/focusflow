e1f1600370601056661ed4c060680fa52b35dba7
Max Trautwein <trautwein.max@gmail.com>
2025-03-28 23:00:24 +0100

    fix missing in last commit

--- backend/focusflow/src/main/java/hse/group1/focusflow/repository/TaskRepository.java
+++ backend/focusflow/src/main/java/hse/group1/focusflow/repository/TaskRepository.java
@@ -1,4 +1,9 @@
 package hse.group1.focusflow.repository;
 
-public class TaskRepository {
+import hse.group1.focusflow.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task,Long> {
 }
--- backend/focusflow/src/main/java/hse/group1/focusflow/repository/TeamRepository.java
+++ backend/focusflow/src/main/java/hse/group1/focusflow/repository/TeamRepository.java
@@ -1,4 +1,7 @@
 package hse.group1.focusflow.repository;
 
-public class TeamRepository {
+import hse.group1.focusflow.model.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamRepository extends JpaRepository<Team,Long> {
 }
