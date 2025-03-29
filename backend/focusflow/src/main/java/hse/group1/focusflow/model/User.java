5d26f8d03b29059e1e027be2bb3bb7d7c4a7b036
Tim Jauch <tijait00@hs-esslingen.de>
2025-03-29 23:09:26 +0100

    Changed position of already implemented helper methods.

--- backend/focusflow/src/main/java/hse/group1/focusflow/model/User.java
+++ backend/focusflow/src/main/java/hse/group1/focusflow/model/User.java
@@ -1,10 +1,9 @@
 package hse.group1.focusflow.model;
 
 import jakarta.persistence.*;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
-
 import java.time.Instant;
 import java.util.List;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 
 @Entity
 @Table(name = "\"user\"") // user is reserved in postgres -> must quote name
@@ -32,14 +31,18 @@ public class User {
   @OneToMany(mappedBy = "user")
   private List<Task> tasks;
 
-
   /**
    * @param email E-Mail of the User
    * @param password Clear Text PW (Will be hashed & Salted)
    * @param first_name First Name of User
    * @param last_name Last Name of user
    */
-  public User(String email, String password, String first_name, String last_name) {
+  public User(
+    String email,
+    String password,
+    String first_name,
+    String last_name
+  ) {
     this.email = email;
     this.setPassword(password);
     this.first_name = first_name;
@@ -47,9 +50,7 @@ public class User {
     this.created_at = Instant.now();
   }
 
-  public User() {
-
-  }
+  public User() {}
 
   // Getters and Setters
 
@@ -64,25 +65,6 @@ public class User {
 
   public void setEmail(String email) {
     this.email = email;
-  }
-
-
-  /** Checks if a Given PW is correct
-   * @param password Plain text PW
-   * @return True if the Password is correct
-   */
-  public boolean passwordMatches(String password) {
-    Argon2PasswordEncoder encoder = Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
-    return encoder.matches(this.password, password);
-  }
-
-  /**
-   * Save / Set a Password and Hash it with a salt
-   * @param password Password to be Hashed
-   */
-  public void setPassword(String password) {
-    Argon2PasswordEncoder encoder = Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
-    this.password = encoder.encode(password);
   }
 
   public String getFirst_name() {
@@ -109,10 +91,31 @@ public class User {
     return last_login;
   }
 
+  // Helper methods
   /**
    * Updates the last_login with the current Time
    */
   public void updateLast_login() {
     this.last_login = Instant.now();
+  }
+
+  /** Checks if a Given PW is correct
+   * @param password Plain text PW
+   * @return True if the Password is correct
+   */
+  public boolean passwordMatches(String password) {
+    Argon2PasswordEncoder encoder =
+      Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+    return encoder.matches(this.password, password);
+  }
+
+  /**
+   * Save / Set a Password and Hash it with a salt
+   * @param password Password to be Hashed
+   */
+  public void setPassword(String password) {
+    Argon2PasswordEncoder encoder =
+      Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+    this.password = encoder.encode(password);
   }
 }
